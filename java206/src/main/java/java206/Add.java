32f88176f246e97631844b5ed10ff6984391b11e
ashish-ankam <ashishankam1999@gmail.com>
2026-01-27 21:44:04 +0530

    servlet practice.

---
+++ java206/src/main/java/java206/Add.java
@@ -0,0 +1,18 @@
+package java206;
+
+import java.io.IOException;
+
+import jakarta.servlet.GenericServlet;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+
+public class Add extends GenericServlet {
+
+	@Override
+	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
