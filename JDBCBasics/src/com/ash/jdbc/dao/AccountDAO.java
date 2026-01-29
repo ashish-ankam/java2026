a3759d187f2266c7d2a34c91ddee1ff45901f403
ashish-ankam <ashishankam1999@gmail.com>
2026-01-29 20:57:06 +0530

    jdbc basic project , connection establishment , data values  inserted into table name account

--- JDBCBasics/src/com/ash/jdbc/dao/AccountDAO.java
+++ JDBCBasics/src/com/ash/jdbc/dao/AccountDAO.java
@@ -6,13 +6,14 @@ public class AccountDAO {
 		public static void main(String[] args) {
 
 	        try {
-	            Connection con = DriverManager.getConnection(
-	                "jdbc:mysql://localhost:3306/world",
-	                "root",
-	                "*"
-	            );
+	            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb","root","*");
 
 	            System.out.println("Database connected successfully");
+	            
+	            Statement statement = con.createStatement();
+	            
+	            int result = statement.executeUpdate("insert into account values(2,'Spider','Man',200000)");
+	            System.out.println(result + " rows inserted");
 
 	        } catch (Exception e) {
 	            e.printStackTrace();
