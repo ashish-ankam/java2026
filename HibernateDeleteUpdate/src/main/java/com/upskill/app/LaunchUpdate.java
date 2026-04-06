9d9d458945fa12ef42729aeecbd4c4f8088f0cab
ashish-ankam <ashishankam1999@gmail.com>
2026-04-06 14:43:20 +0530

    update method -> only if there is any update , if we give new value it wont save ,So here we can use saveOrUpdate() method

--- HibernateDeleteUpdate/src/main/java/com/upskill/app/LaunchUpdate.java
+++ HibernateDeleteUpdate/src/main/java/com/upskill/app/LaunchUpdate.java
@@ -1,7 +1,9 @@
 package com.upskill.app;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 
 import com.upskill.model.Student;
@@ -13,7 +15,40 @@ public class LaunchUpdate {
 		
 	  SessionFactory sessionFactory = new Configuration().configure().addAnnotatedClass(Student.class).buildSessionFactory();
       Session session = null;
+      Transaction transaction=null;
+      boolean flag=false;
       
+      try {
+    	  session = sessionFactory.openSession();
+    	  transaction = session.beginTransaction();
+    	  Student student = new Student();
+    	  student.setId(3);
+  		student.setsAge(24);
+  		student.setsCity("hyderabad");
+  		student.setsName("kohli");
+  		
+  		//session.update(student);
+  	session.saveOrUpdate(student);
+  		
+  		flag=true;
+      }
+      catch(HibernateException e) {
+    	  e.printStackTrace();
+      }
+      catch(Exception e) {
+    	  e.printStackTrace();
+      }
+      finally {
+    	  if(flag)
+    	  {
+    		  transaction.commit();
+    	  }
+    	  else {
+    		  transaction.rollback();
+    	  }
+    	  session.close();
+    	  sessionFactory.close();
+      }
       
 	  
 	}
